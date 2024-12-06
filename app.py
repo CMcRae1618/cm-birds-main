fa022c69b7f3389a484d6a3644e4a9d49886554d
CMcRae1618 <144840743+CMcRae1618@users.noreply.github.com>
2024-12-05 19:19:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,31 +3,29 @@ import requests
 import json
 import sqlite3
 
-
 app = Flask(__name__)
-
 
 def get_bird(state: str):
     conn = sqlite3.connect("./birds.db")
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
-    print(f"select * from birds where abbreviation = '{state}';")
-    row = cursor.execute(f"select * from birds where abbreviation = '{state}';")
+    sql = f"select * from birds where abbreviation = '{state.upper()}';"
+    row = cursor.execute(sql)
     res = row.fetchall()
     list_accumulator = []
+    print("Number of results:" + str(len(res)))
     for item in res:
-        print(item)
         list_accumulator.append({k: item[k] for k in item.keys()})
     return json.dumps(list_accumulator)
 
-
 def get_weather(state: str):
-    r = requests.get(f'https://api.weather.gov/alerts/active?area={{abbreviation}}')
+    request_url = f"https://api.weather.gov/alerts/active?area={state.upper()}"
+    r = requests.get(request_url)
     return r.json()
 
 
 @app.get('/')
-def hello():
+def birds_and_weather():
     return "Add a 2 letter state param to learn about birds and the weather challenges they face.", \
            200, \
            {'Content-Type': 'text/html; charset=utf-8'}
@@ -35,11 +33,9 @@ def hello():
 
 @app.get('/<state>')
 def bird(state):
-
-    bird = get_bird(state)
-    print(bird)
-    weather = get_weather(state)
-    print(weather)
-    out = str([bird, weather])
-    return out, 200, {'Content-Type': 'application/json'}
-
+    bird_info = get_bird(state)
+    weather_info = get_weather(state)
+    return {"status": 200,"bird_information": bird_info,"weather_information": weather_info}
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
